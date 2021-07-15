85071330562177ef96563892963d7e74fa6b3618
Lucas Bassetti <lucasbr.dafonseca@gmail.com>
2021-07-15 14:52:28 -0300

    Add import lint

--- next.config.js
+++ next.config.js
@@ -7,7 +7,4 @@ module.exports = withPWA({
     dest: 'public',
     disable: !isProd,
   },
-  future: {
-    webpack5: true,
-  },
 });
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
+
 import GlobalStyles from 'styles/global';
 import theme from 'styles/theme';
 
