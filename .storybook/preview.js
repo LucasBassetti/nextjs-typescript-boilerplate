ee8bfa0f79cdc07f57e9bcc545dc7fe91b608cc5
Lucas Bassetti <lucasbr.dafonseca@gmail.com>
2022-04-11 01:19:05 -0300

    update packages

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,7 @@
-import { addDecorator } from '@storybook/react';
-import { withNextRouter } from 'storybook-addon-next-router';
 import { ThemeProvider } from 'styled-components';
+
 import GlobalStyles from 'styles/global';
 import theme from 'styles/theme';
-
-addDecorator(withNextRouter());
 
 export const decorators = [
   (Story) => (
