10ba2a02b967a8f1b22bc0c25cfc3a877f590010
Lucas Bassetti <lucasbr.dafonseca@gmail.com>
2022-10-25 18:38:07 -0300

    feat(packages): update packages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa');
-const isProd = process.env.NODE_ENV === 'production';
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
+});
 
 module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: !isProd,
-  },
+  // next.js config
 });
